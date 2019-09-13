a576c924d389587343fefce1addeb3394e4c727e
Tony <tonyrkovar@gmail.com>
2019-09-13 11:04:34 -0500

    got api request working and have displaying properly is this the right place to commit this

--- starwars/src/components/StarCard.js
+++ starwars/src/components/StarCard.js
@@ -1,1 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
+import axios from 'axios';
+import StarGrid from './StarGrid';
+import styled from 'styled-components';
+
+const ParentDiv = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+`;
+
+function StarCard(){
+    const [starPeople, setStarPeople] = useState([])
+    useEffect(() => {
+        axios
+            .get('https://swapi.co/api/people')
+            .then(response => {
+                const results = response.data.results;
+                console.log(response)
+                setStarPeople(results);
+                console.log('THIS IS results',results)
+            })
+            .catch(error => {
+                console.log('Sorry, there was an error', error)
+            })
+    }, []);
+
+    return (
+        <ParentDiv>
+            {starPeople.map((e,i) => {
+                return <StarGrid 
+                name={e.name}
+                eye={e.eye_color}
+                key={i}
+                vehicles={e.vehicles}
+                films={e.films}
+                />
+            })}
+
+
+        </ParentDiv>
+        
+    )
+
+
+}
+
+export default StarCard;
